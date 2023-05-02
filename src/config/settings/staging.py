cf3c092b7d856b0a9c8e32b1bd3558fe368b826a
Netrobe Ubuntu <netrobepy@gmail.com>
2023-05-02 12:36:36 +0100

    updated staging enviroment

--- src/config/settings/staging.py
+++ src/config/settings/staging.py
@@ -4,9 +4,7 @@ ALLOWED_HOSTS = ['roadflow.tripvalue.com.ng']
 
 STATIC_ROOT = BASE_DIR / "static"  # noqa
 
-SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=200)  # noqa
-}
+SIMPLE_JWT['ACCESS_TOKEN_LIFETIME'] = timedelta(minutes=200)  # noqa
 
 PRINT_LOG = False
 OFF_EMAIL = False
